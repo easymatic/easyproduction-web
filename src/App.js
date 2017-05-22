a5d25e12385fbc1d9318a702345826b6c6f19b28
Anton Pchelkin <imanbee@gmail.com>
2017-05-22 13:06:28 +0300

    React Redux initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h2>easyproduction</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
